1acc87e9ffdb0bac0095efc6ecd2e3325a270360
daniel-kav <dankavatha01@gmail.com>
2024-06-12 21:01:27 +0300

    statuscatalog auth

--- src/statusCatalog/statuscatalog.router.ts
+++ src/statusCatalog/statuscatalog.router.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { statusCatalogSchema} from "../validators";
 import { createstatusCatalog, deletestatusCatalog, getstatusCatalog, liststatusCatalog, updatestatusCatalog } from "./statuscatalog.controller";
-import { adminOrUserRoleAuth } from "../middleware/bearAuth";
+import { adminOrUserRoleAuth, adminRoleAuth } from "../middleware/bearAuth";
 export const statusCatalogRouter = new Hono();
 
 //get all address      
@@ -14,9 +14,9 @@ statusCatalogRouter.post("/statusCatalog", zValidator('json', statusCatalogSchem
     if (!result.success) {
         return c.json(result.error, 400)
     }
-}), createstatusCatalog)
+}), adminRoleAuth , createstatusCatalog)
 
 // update a status
-statusCatalogRouter.put("/statusCatalog/:id", updatestatusCatalog)
+statusCatalogRouter.put("/statusCatalog/:id", adminRoleAuth , updatestatusCatalog)
 // delete  a status
-statusCatalogRouter.delete("/statusCatalog/:id", deletestatusCatalog)
+statusCatalogRouter.delete("/statusCatalog/:id", adminRoleAuth , deletestatusCatalog)
