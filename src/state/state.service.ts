0d5e2a1c74cc5490cc0f228d22d96d357294124e
daniel-kav <dankavatha01@gmail.com>
2024-06-16 19:50:58 +0300

    Typed state services return types

--- src/state/state.service.ts
+++ src/state/state.service.ts
@@ -19,25 +19,32 @@ export const getstateService = async (id: number): Promise<TIState[] | unknown >
     })
 }
 
-export const createstateService = async (User: TIState) => {
+export const createstateService = async (User: TIState): Promise < string > => {
     await db.insert(state).values(User)
     return "state created successfully";
 }
 
-export const updatestateService = async (id: number, userData: TIState) => {
+export const updatestateService = async (id: number, userData: TIState): Promise < string > => {
     await db.update(state).set(userData).where(eq(state.id, id))
     return "state updated successfully";
 }
 
-export const deletestateService = async (id: number) => {
+export const deletestateService = async (id: number): Promise < string > => {
     await db.delete(state).where(eq(state.id, id))
     return "state deleted successfully";
 }
 
+// Define a type for the city data returned by the service
+export interface City {
+  name: string;
+}
 // Service to fetch cities by state ID
-export const getCitiesByStateService = async (stateId: number) => {
+
+export const getCitiesByStateService = async (stateId: number): Promise<City[]> => {
   const cities = await db
-    .select()
+    .select({
+      name: city.name, // Select only the name field
+    })
     .from(city)
     .where(eq(city.stateId, stateId))
     .execute();
@@ -45,13 +52,24 @@ export const getCitiesByStateService = async (stateId: number) => {
   return cities;
 };
 
-// Service to fetch restaurants by city ID
-export const getRestaurantsByCityService = async (cityId: number) => {
-  const restaurants = await db
-    .select()
+
+// Define a type or interface for the  restaurant data
+export interface Restaurant {
+  name: string;
+  zipCode: string;
+  streetAddress: string;
+}
+
+// Service function to fetch restaurants by city ID
+export const getRestaurantsByCityService = async (cityId: number): Promise<Restaurant[]> => {
+  return await db
+    .select({
+      name: restaurant.name,
+      zipCode: restaurant.zipCode,
+      streetAddress: restaurant.streetAddress,
+    })
     .from(restaurant)
     .where(eq(restaurant.cityId, cityId))
     .execute();
-
-  return restaurants;
 };
+
